b33d72f8d200b409714095053215cb2a5428ae76
SivaenshM <shivaaarthi08@gmail.com>
2022-11-17 13:34:45 +0400

    Comments added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Card from './Card';
 
 function App() {
   return (
+    // JSX Card Section Starts
     <section className="pricing py-5">
       <div className="container">
+        {/* Task Title */}
         <h1>Price cart - React</h1>
         <div className="row">
+          {/* Card Component  with props name and price*/}
           <Card cardName ="FREE" price="0" />
           <Card cardName ="PLUS" price="9"/>
           <Card cardName ="PRO" price="49"/>
